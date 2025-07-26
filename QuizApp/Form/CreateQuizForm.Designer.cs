53860b58bc3e3300f7983eea7e4d51f8fd886f0e
envyay <viannguyen119@gmail.com>
2025-07-26 15:54:59 +0700

    WIP

---
+++ QuizApp/Form/CreateQuizForm.Designer.cs
@@ -0,0 +1,75 @@
+﻿using System.ComponentModel;
+
+namespace QuizApp;
+
+partial class CreateQuizForm
+{
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+        BackBtn = new System.Windows.Forms.Button();
+        TypeCB = new System.Windows.Forms.ComboBox();
+        SuspendLayout();
+        // 
+        // BackBtn
+        // 
+        BackBtn.Location = new System.Drawing.Point(12, 12);
+        BackBtn.Name = "BackBtn";
+        BackBtn.Size = new System.Drawing.Size(100, 28);
+        BackBtn.TabIndex = 0;
+        BackBtn.Text = "Back";
+        BackBtn.UseVisualStyleBackColor = true;
+        BackBtn.Click += BackBtn_Click;
+        // 
+        // TypeCB
+        // 
+        TypeCB.FormattingEnabled = true;
+        TypeCB.Items.AddRange(new object[] { "Multiple Choice", "True/False", "Open Ended" });
+        TypeCB.Location = new System.Drawing.Point(18, 69);
+        TypeCB.Name = "TypeCB";
+        TypeCB.Size = new System.Drawing.Size(138, 23);
+        TypeCB.TabIndex = 1;
+        TypeCB.Text = "Question Type";
+        TypeCB.SelectedIndexChanged += TypeCB_SelectedIndexChanged;
+        // 
+        // CreateQuizForm
+        // 
+        AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+        AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        ClientSize = new System.Drawing.Size(800, 450);
+        Controls.Add(TypeCB);
+        Controls.Add(BackBtn);
+        Text = "CreateQuizForm";
+        ResumeLayout(false);
+    }
+
+    private System.Windows.Forms.ComboBox TypeCB;
+
+    private System.Windows.Forms.Button BackBtn;
+
+    #endregion
+}
---
+++ QuizApp/Form/CreateQuizForm.cs
@@ -0,0 +1,36 @@
+﻿using QuizApp.Common;
+
+namespace QuizApp;
+
+public partial class CreateQuizForm : Form
+{
+    public CreateQuizForm()
+    {
+        InitializeComponent();
+    }
+
+    private void BackBtn_Click(object sender, EventArgs e)
+    {
+        var mainForm = new MainForm();
+        mainForm.Show();
+        this.Close();
+    }
+
+    private void TypeCB_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        var typeName = TypeCB.SelectedItem?.ToString() ?? "";
+        var typeId = Constants.QuestionType(typeName);
+
+        switch (typeId)
+        {
+            case Constants.MULTIPLE_CHOICE:
+                break;
+            case Constants.TRUE_FALSE:
+                break;
+            case Constants.OPEN_ENDED:
+                break;
+            default:
+                break;
+        }
+    }
+}
